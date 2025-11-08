9fb33ab04c0dca301fdd7eaae15851b29666e7fc
Ammar-Taha <ammartaha735@yahoo.com>
2025-11-08 12:24:04 +0200

    feat: make logo link to homepage

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./styles/main.css";
+import logoUrl from "./assets/shared/logo.svg";
 import { initApp } from "./app.js";
 
 // Initialize app when DOM is ready
@@ -6,4 +7,14 @@ if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", initApp);
 } else {
   initApp();
+}
+
+const logoImg = document.querySelector("[data-logo]");
+if (logoImg) {
+  logoImg.src = logoUrl;
+}
+
+const favicon = document.getElementById("app-favicon");
+if (favicon) {
+  favicon.href = logoUrl;
 }
